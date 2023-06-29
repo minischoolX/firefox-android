b6c583a2d344c5a1fb5e19e46213313a979e94a1
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-06-29 09:56:11 -0400

    Bug 1841081 - Update Kotlin Coroutines to version 1.7.2

--- focus-android/plugins/focusdependencies/src/main/java/FocusDependenciesPlugin.kt
+++ focus-android/plugins/focusdependencies/src/main/java/FocusDependenciesPlugin.kt
@@ -30,10 +30,6 @@ object FocusVersions {
         const val play = "1.10.3"
     }
 
-    object Kotlin {
-        const val coroutines = "1.7.1"
-    }
-
     object Testing {
         const val androidx_espresso = "3.5.1"
         const val androidx_orchestrator = "1.4.2"
@@ -60,8 +56,6 @@ object FocusDependencies {
 
     const val google_play = "com.google.android.play:core:${FocusVersions.Google.play}"
     const val leakcanary = "com.squareup.leakcanary:leakcanary-android-core:${FocusVersions.ThirdParty.leakcanary}"
-
-    const val kotlin_coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${FocusVersions.Kotlin.coroutines}"
 
     const val adjust = "com.adjust.sdk:adjust-android:${FocusVersions.Adjust.adjust}"
     const val install_referrer = "com.android.installreferrer:installreferrer:${FocusVersions.Adjust.install_referrer}"
